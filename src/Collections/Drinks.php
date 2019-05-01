f5836e800cdb07f906ab9a3405b249a41872e0a5
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-04-30 18:44:14 -0700

    Fixed Collection classes not behaving like a Collection in all circumstances

--- src/Collections/Drinks.php
+++ src/Collections/Drinks.php
@@ -6,39 +6,44 @@ use Tightenco\Collect\Support\Collection;
 
 final class Drinks extends Collection
 {
+    /** @var array The items contained in the collection. */
+    protected $items = [
+        'affogato',
+        'americano',
+        'breve',
+        'cafe-au-lait',
+        'cafe-crema',
+        'cafe-zorro',
+        'cappuccino',
+        'coffee',
+        'cold-brew',
+        'cortado',
+        'cubano',
+        'drip-coffee',
+        'espresso',
+        'flat-white',
+        'frappe',
+        'frappuccino',
+        'french-press-coffee',
+        'iced-coffee',
+        'irish-coffee',
+        'latte',
+        'lungo',
+        'macchiato',
+        'mocha',
+        'pour-over',
+        'ristretto'
+    ];
+
     /**
-     * Create a new Drinks collection.
+     * Create a new Varriants collection.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($items = null)
     {
-        parent::__construct([
-            'affogato',
-            'americano',
-            'breve',
-            'cafe-au-lait',
-            'cafe-crema',
-            'cafe-zorro',
-            'cappuccino',
-            'coffee',
-            'cold-brew',
-            'cortado',
-            'cubano',
-            'drip-coffee',
-            'espresso',
-            'flat-white',
-            'frappe',
-            'frappuccino',
-            'french-press-coffee',
-            'iced-coffee',
-            'irish-coffee',
-            'latte',
-            'lungo',
-            'macchiato',
-            'mocha',
-            'pour-over',
-            'ristretto'
-        ]);
+        if (isset($items)) {
+            parent::__construct($items);
+        }
     }
 }
--- src/Collections/Varriants.php
+++ src/Collections/Varriants.php
@@ -6,47 +6,52 @@ use Tightenco\Collect\Support\Collection;
 
 final class Varriants extends Collection
 {
+    /** @var array The items contained in the collection. */
+    protected $items = [
+        'almond',
+        'brown-sugar',
+        'butterscotch',
+        'caramel',
+        'chestnut',
+        'chocolate',
+        'cinnamon',
+        'cocolate-chip',
+        'coconut',
+        'creme-de-menth',
+        'decaf',
+        'double',
+        'french-vanilla',
+        'gingerbread',
+        'grande',
+        'hazelnut',
+        'macadamia',
+        'marshmallow',
+        'mint',
+        'mocha',
+        'peanut-butter',
+        'peppermint',
+        'pumpkin-spice',
+        'salted-caramel',
+        'short',
+        'single',
+        'tall',
+        'toffee',
+        'trenta',
+        'triple',
+        'vanilla',
+        'venti',
+        'white-chocolate'
+    ];
+
     /**
      * Create a new Varriants collection.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($items = null)
     {
-        parent::__construct([
-            'almond',
-            'brown-sugar',
-            'butterscotch',
-            'caramel',
-            'chestnut',
-            'chocolate',
-            'cinnamon',
-            'cocolate-chip',
-            'coconut',
-            'creme-de-menth',
-            'decaf',
-            'double',
-            'french-vanilla',
-            'gingerbread',
-            'grande',
-            'hazelnut',
-            'macadamia',
-            'marshmallow',
-            'mint',
-            'mocha',
-            'peanut-butter',
-            'peppermint',
-            'pumpkin-spice',
-            'salted-caramel',
-            'short',
-            'single',
-            'tall',
-            'toffee',
-            'trenta',
-            'triple',
-            'vanilla',
-            'venti',
-            'white-chocolate'
-        ]);
+        if (isset($items)) {
+            parent::__construct($items);
+        }
     }
 }
