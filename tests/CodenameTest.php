0041166dc455ff8840c1c3bd1b2b41467f666675
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2019-05-03 22:22:19 -0700

    Fixed failing tests

--- tests/CodenameTest.php
+++ tests/CodenameTest.php
@@ -9,20 +9,20 @@ class CodenameTest extends TestCase
     {
         $codename = new Codename();
 
-        $this->assertRegexp('/^([a-z]+)(?:-([a-z]+))+$/', $codename);
+        $this->assertRegexp('/^([a-z]+)(?:-([a-z]+))+$/', (string) $codename);
     }
 
     public function test_it_can_generate_a_codename_staticaly()
     {
         $codename = Codename::make();
 
-        $this->assertRegexp('/^([a-z]+)(?:-([a-z]+))+$/', $codename);
+        $this->assertRegexp('/^([a-z]+)(?:-([a-z]+))+$/', (string) $codename);
     }
 
     public function test_it_can_generate_a_codename_with_the_helper_method()
     {
         $codename = cafelias();
 
-        $this->assertRegexp('/^([a-z]+)(?:-([a-z]+))+$/', $codename);
+        $this->assertRegexp('/^([a-z]+)(?:-([a-z]+))+$/', (string) $codename);
     }
 }
